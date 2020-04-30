4bbd37dd94faa8890375e15866964fca14e6e792
kmkzt <info.pscreator@gmail.com>
2020-04-30 23:56:35 +0900

    feat: create drawing class as wasm module wrapper

---
+++ example/drawing.ts
@@ -0,0 +1,137 @@
+import { throttle } from './throttle'
+
+interface Point {
+  x: number
+  y: number
+}
+interface DrawingOption {
+  pathClose?: boolean
+  pathCirculer?: boolean
+  delay?: number
+}
+
+export class Drawing {
+  public pathClose = false
+
+  public pathCirculer = false
+
+  public delay: number
+
+  private el: HTMLElement
+
+  private drawable = false
+
+  private stopListener: any
+
+  private wpath: any
+
+  private app: any
+
+  // Wasm instance
+  private static Mod: any = null
+
+  constructor(
+    el: HTMLElement,
+    { pathClose, pathCirculer, delay }: DrawingOption
+  ) {
+    // set parameter
+    this.el = el
+    this.pathClose = pathClose ?? false
+    this.pathCirculer = pathCirculer ?? false
+    this.delay = delay ?? 20
+
+    // bind methods
+    this.init = this.init.bind(this)
+    this.handleMouse = this.handleMouse.bind(this)
+    this.drawStart = this.drawStart.bind(this)
+    this.drawMove = this.drawMove.bind(this)
+    this.drawEnd = this.drawEnd.bind(this)
+    this.startListener = this.startListener.bind(this)
+
+    // Load Drawing WASM Module
+    if (!Drawing.Mod) {
+      import('../pkg')
+        .then((mod: any): void => {
+          Drawing.Mod = mod
+          this.init()
+        })
+        .catch(console.error)
+    }
+  }
+
+  public init() {
+    const { width, height } = this.el.getBoundingClientRect()
+    this.app = Drawing.Mod.SvgDrawing.new(width, height)
+    this.stopListener = this.startListener()
+  }
+
+  public changeThrottle(ms: number) {
+    this.delay = ms
+    if (this.stopListener) {
+      this.stopListener()
+    }
+    this.stopListener = this.startListener()
+  }
+
+  public clear() {
+    this.app.clear()
+    this.render()
+  }
+
+  private handleMouse(cb: (arg: Point) => void) {
+    return (ev: MouseEvent): void => {
+      const rect = this.el.getBoundingClientRect()
+      cb({ x: ev.clientX - rect.left, y: ev.clientY - rect.top })
+    }
+  }
+
+  private drawStart({ x, y }: Point) {
+    this.wpath = Drawing.Mod.SvgPath.new(this.pathClose, this.pathCirculer)
+    this.wpath.add(Drawing.Mod.Point.new(x, y))
+    this.app.add(this.wpath.copy())
+    this.el.innerHTML = this.app.to_string()
+    this.drawable = true
+    console.log('START: x', x, 'y', y)
+  }
+
+  private drawMove({ x, y }: Point) {
+    if (!this.drawable) return
+    this.wpath.add(Drawing.Mod.Point.new(x, y))
+    this.app.update(this.wpath.copy())
+    this.render()
+    console.log('MOVE: x', x, 'y', y, this.wpath.isClose())
+  }
+
+  private drawEnd({ x, y }: Point) {
+    this.drawable = false
+    this.wpath.add(Drawing.Mod.Point.new(x, y))
+    this.app.update(this.wpath)
+    this.render()
+    console.log('END: x', x, 'y', y)
+  }
+
+  /**
+   * TODO: add Pointer or Touch Listener
+   */
+  private startListener() {
+    const handleMouseDown = this.handleMouse(this.drawStart)
+    const handleMouseMove = throttle(
+      this.handleMouse(this.drawMove),
+      this.delay
+    )
+    const handleMouseUp = this.handleMouse(this.drawEnd)
+    this.el.addEventListener('mousedown', handleMouseDown)
+    this.el.addEventListener('mousemove', handleMouseMove)
+    this.el.addEventListener('mouseup', handleMouseUp)
+
+    return (): void => {
+      this.el.removeEventListener('mousedown', handleMouseDown)
+      this.el.removeEventListener('mousemove', handleMouseMove)
+      this.el.removeEventListener('mouseup', handleMouseUp)
+    }
+  }
+
+  private render() {
+    this.el.innerHTML = this.app.to_string()
+  }
+}
--- example/index.ts
+++ example/index.ts
@@ -1,137 +1,60 @@
-import { throttle } from './throttle'
+import { Drawing } from './drawing'
 
 const WIDTH = 500
 const HEIGHT = 500
+const main = () => {
+  const el = document.getElementById('app')
+  if (!el) return
 
-let throttleDelay = 150
-let closePath = false
-let circulPath = false
-
-import('../pkg')
-  .then(
-    ({
-      // renderDraw, // RustEventHandler
-      SvgDrawing,
-      SvgPath,
-      Point,
-    }: any): void => {
-      // Rust EventHandler
-      // const rustDrawing = (elementId: string): void => {
-      //   const app = document.getElementById(elementId)
-      //   app.setAttribute(
-      //     'style',
-      //     `width: ${WIDTH}; height: ${HEIGHT}; border: 1px solid orange;`
-      //   )
-      //   renderDraw(elementId)
-      // }
-      // rustDrawing('app')
-
-      /**
-       * Draw app
-       */
-      const el = document.createElement('div')
-      el.setAttribute(
-        'style',
-        `width: ${WIDTH}; height: ${HEIGHT}; border: 1px solid #000;`
-      )
-      const drawApp: any = SvgDrawing.new(WIDTH, HEIGHT)
-      let wpath: any = null
-      let drawable = false
-      const handlePoint = (cb: (arg: { x: number; y: number }) => void) => (
-        ev: MouseEvent
-      ): void => {
-        const rect = el.getBoundingClientRect()
-        const x = ev.clientX - rect.left
-        const y = ev.clientY - rect.top
-        cb({ x, y })
-      }
-      const setupDrawing = (): (() => void) => {
-        const handleMouseDown = handlePoint(({ x, y }) => {
-          wpath = SvgPath.new(closePath, circulPath)
-          wpath.add(Point.new(x, y))
-          drawApp.add(wpath.copy())
-          el.innerHTML = drawApp.to_string()
-          drawable = true
-          console.log('START: x', x, 'y', y)
-        })
-
-        const handleMouseMove = throttle(
-          handlePoint(({ x, y }) => {
-            if (!drawable) return
-            wpath.add(Point.new(x, y))
-            drawApp.update(wpath.copy())
-            el.innerHTML = drawApp.to_string()
-            console.log('MOVE: x', x, 'y', y, wpath.isClose())
-          }),
-          throttleDelay
-        )
-
-        const handleMouseUp = handlePoint(({ x, y }) => {
-          drawable = false
-          wpath.add(Point.new(x, y))
-          drawApp.update(wpath)
-          el.innerHTML = drawApp.to_string()
-          console.log('END: x', x, 'y', y)
-        })
-        el.addEventListener('mousedown', handleMouseDown)
-        el.addEventListener('mousemove', handleMouseMove)
-        el.addEventListener('mouseup', handleMouseUp)
-
-        return (): void => {
-          el.removeEventListener('mousedown', handleMouseDown)
-          el.removeEventListener('mousemove', handleMouseMove)
-          el.removeEventListener('mouseup', handleMouseUp)
-        }
-      }
-      let resetDawing = setupDrawing()
-
-      document.body.appendChild(el)
-      /**
-       * ClearButton
-       */
-      const clearBtn = document.createElement('button')
-      clearBtn.innerHTML = 'clear'
-      clearBtn.addEventListener('click', () => {
-        drawApp.clear()
-        el.innerHTML = drawApp.to_string()
-      })
-      document.body.appendChild(clearBtn)
-      /**
-       * Toggle Path Close Button
-       */
-      const toggleCloseButton = document.createElement('button')
-      toggleCloseButton.innerHTML = 'toggle close path'
-      toggleCloseButton.addEventListener('click', () => {
-        closePath = !closePath
-      })
-      document.body.appendChild(toggleCloseButton)
-      /**
-       * Toggle Path Circul Button
-       */
-      const togglCirculButton = document.createElement('button')
-      togglCirculButton.innerHTML = 'toggle circul path'
-      togglCirculButton.addEventListener('click', () => {
-        circulPath = !circulPath
-      })
-      document.body.appendChild(togglCirculButton)
-      /**
-       * Toggle Path Circul Button
-       */
-      const displayThrottle = document.createElement('span')
-      displayThrottle.innerHTML = `throttle: ${String(throttleDelay)}`
-      const changeThrottle = document.createElement('input')
-      changeThrottle.setAttribute('type', 'range')
-      changeThrottle.setAttribute('min', '0')
-      changeThrottle.setAttribute('max', '300')
-      changeThrottle.setAttribute('step', '5')
-      changeThrottle.addEventListener('change', (e: any) => {
-        throttleDelay = Number(e.target.value)
-        displayThrottle.innerHTML = `throttle: ${String(throttleDelay)}`
-        resetDawing()
-        resetDawing = setupDrawing()
-      })
-      document.body.appendChild(changeThrottle)
-      document.body.appendChild(displayThrottle)
-    }
+  const drawing = new Drawing(el, {})
+  el.setAttribute(
+    'style',
+    `width: ${WIDTH}; height: ${HEIGHT}; border: 1px solid #000;`
   )
-  .catch(console.error)
+
+  /**
+   * ClearButton
+   */
+  const clearBtn = document.createElement('button')
+  clearBtn.innerHTML = 'clear'
+  clearBtn.addEventListener('click', () => {
+    drawing.clear()
+  })
+  document.body.appendChild(clearBtn)
+  /**
+   * Toggle Path Close Button
+   */
+  const toggleCloseButton = document.createElement('button')
+  toggleCloseButton.innerHTML = 'toggle close path'
+  toggleCloseButton.addEventListener('click', () => {
+    drawing.pathClose = !drawing.pathClose
+  })
+  document.body.appendChild(toggleCloseButton)
+  /**
+   * Toggle Path Circul Button
+   */
+  const togglCirculButton = document.createElement('button')
+  togglCirculButton.innerHTML = 'toggle circul path'
+  togglCirculButton.addEventListener('click', () => {
+    drawing.pathCirculer = !drawing.pathCirculer
+  })
+  document.body.appendChild(togglCirculButton)
+  /**
+   * Toggle Path Circul Button
+   */
+  const displayThrottle = document.createElement('span')
+  displayThrottle.innerHTML = `throttle: ${String(drawing.delay)}`
+  const changeThrottle = document.createElement('input')
+  changeThrottle.setAttribute('type', 'range')
+  changeThrottle.setAttribute('min', '0')
+  changeThrottle.setAttribute('max', '300')
+  changeThrottle.setAttribute('step', '5')
+  changeThrottle.addEventListener('change', (e: any) => {
+    drawing.changeThrottle(Number(e.target.value))
+    displayThrottle.innerHTML = `throttle: ${String(drawing.delay)}`
+  })
+  document.body.appendChild(changeThrottle)
+  document.body.appendChild(displayThrottle)
+}
+
+main()
