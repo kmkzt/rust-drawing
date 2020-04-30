6c94d91d1e596be3d9a26400a0adb740ce01ccc3
kmkzt <info.pscreator@gmail.com>
2020-04-30 10:33:41 +0900

    feat: add svgpath test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // refference: https://github.com/rustwasm/wasm-bindgen/tree/master/examples/paint
+use std::ops::{Add, Sub};
 use std::option::Option;
 use wasm_bindgen::prelude::*;
 
@@ -16,13 +17,14 @@ use wasm_bindgen::prelude::*;
 // }
 
 #[wasm_bindgen]
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 struct Point {
     x: f32,
     y: f32,
 }
 
 #[wasm_bindgen]
+#[derive(Copy, Clone, Debug, PartialEq)]
 struct Vector {
     value: f32,
     angle: f32,
@@ -32,7 +34,14 @@ impl Vector {
     pub fn point(&self) -> Point {
         Point {
             x: self.angle.cos() * self.value,
-            y: self.angle.sin() * self.value
+            y: self.angle.sin() * self.value,
+        }
+    }
+
+    pub fn scale(&self, s: f32) -> Self {
+        Vector {
+            angle: self.angle,
+            value: self.value * s,
         }
     }
 }
@@ -44,26 +53,91 @@ impl Point {
     }
 
     pub fn vector(&self, p: &Point) -> Vector {
-        let vx = p.x - self.x;
-        let vy = p.y - self.y;
-
+        let v = p.sub(*self);
         Vector {
-            value: (vx.powf(2.0) + vy.powf(2.0)).sqrt(),
-            angle: vy.atan2(vx),
+            value: (v.x.powf(2.0) + v.y.powf(2.0)).sqrt(),
+            angle: v.y.atan2(v.x),
+        }
+    }
+
+    pub fn scale(&self, r: f32) -> Self {
+        Point {
+            x: self.x * r,
+            y: self.y * r,
+        }
+    }
+
+    pub fn control(&self, v: &Vector) -> Self {
+        v.point().add(*self)
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
         }
     }
 }
 
-const SMOOTH_RATIO: f32 = 0.2;
-fn create_control_point(prev: &Point, curr: &Point, next: &Point) -> Point {
-    let control = prev.vector(next).point();
+impl Sub for Point {
+    type Output = Point;
 
-    Point {
-        x: curr.x + control.x * SMOOTH_RATIO,
-        y: curr.y + control.y * SMOOTH_RATIO,
+    fn sub(self, other: Point) -> Point {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
     }
 }
+
+#[test]
+fn test_point() {
+    // vector
+    {
+        assert_eq!(
+            Point::new(0.0, 0.0).vector(&Point::new(1.0, 1.0)),
+            Vector {
+                value: 1.4142135,
+                angle: 0.7853982
+            }
+        );
+
+        // TODO: Fix to convert Point { x: 1.0, y: 1.0 }
+        assert_eq!(
+            Vector {
+                value: 1.4142135,
+                angle: 0.7853982
+            }
+            .point(),
+            Point {
+                x: 0.99999994,
+                y: 0.99999994
+            }
+        );
+    }
+    // control
+    {
+        let prev = Point { x: 1.0, y: 1.0 };
+        let curr = Point { x: 2.0, y: 2.0 };
+        let next = Point { x: 3.0, y: 1.0 };
+        let vector = prev.vector(&next).scale(0.2);
+        assert_eq!(curr.control(&vector), Point { x: 2.4, y: 2.0 });
+    }
+    // scale
+    {
+        assert_eq!(
+            Point { x: 1.0, y: 1.0 }.scale(0.2),
+            Point { x: 0.2, y: 0.2 }
+        )
+    }
+}
+
 fn create_path(line: Vec<Point>, close: bool, circul: bool) -> String {
+    const SMOOTH_RATIO: f32 = 0.2;
     let mut path_d = "".to_string();
     for (i, po) in line.iter().enumerate() {
         // Start
@@ -82,8 +156,8 @@ fn create_path(line: Vec<Point>, close: bool, circul: bool) -> String {
                 let p1 = line[i - 1];
                 let p2 = line[i - 2];
                 let n = line[i + 1];
-                let cl = create_control_point(&p2, &p1, &po);
-                let cr = create_control_point(&n, &po, &p1);
+                let cl = &p1.control(&p2.vector(&po).scale(SMOOTH_RATIO));
+                let cr = &po.control(&n.vector(&p1).scale(SMOOTH_RATIO));
                 path_d.push_str(&format!(
                     " C {} {} {} {} {} {}",
                     cl.x, cl.y, cr.x, cr.y, po.x, po.y
@@ -122,7 +196,7 @@ fn test_create_path() {
 
 // TODO: add fill, stroke, storke-width
 #[wasm_bindgen]
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct SvgPath {
     close: bool,
     circul: bool,
@@ -132,15 +206,14 @@ struct SvgPath {
 #[wasm_bindgen]
 impl SvgPath {
     pub fn new(close: Option<bool>, circul: Option<bool>) -> Self {
-
         SvgPath {
             close: match close {
                 Some(clo) => clo,
-                None => false
-            }  ,
+                None => false,
+            },
             circul: match circul {
                 Some(cir) => cir,
-                None => false
+                None => false,
             },
             d: Vec::new(),
         }
@@ -152,7 +225,7 @@ impl SvgPath {
     }
 
     #[wasm_bindgen(js_name=toggleCircul)]
-    pub fn toggle_circul(&mut self)  {
+    pub fn toggle_circul(&mut self) {
         self.circul = !self.circul
     }
 
@@ -162,7 +235,7 @@ impl SvgPath {
     }
 
     #[wasm_bindgen(js_name=toggleClose)]
-    pub fn toggle_close(&mut self)  {
+    pub fn toggle_close(&mut self) {
         self.close = !self.close
     }
 
@@ -186,8 +259,25 @@ impl SvgPath {
     }
 }
 
+#[test]
+fn test_svgpath() {
+    // Polygon Mode
+    assert_eq!(
+        create_path(
+            vec![
+                Point { x: 0.0, y: 0.0 },
+                Point { x: 1.0, y: 1.0 },
+                Point { x: -1.0, y: -1.0 }
+            ],
+            true,
+            false
+        ),
+        "M 0 0 L 1 1 L -1 -1 Z"
+    );
+}
+
 #[wasm_bindgen]
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct SvgDrawing {
     width: u32,
     height: u32,
